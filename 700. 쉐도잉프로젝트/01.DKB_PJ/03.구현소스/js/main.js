a7d2b982f4f2ec6b2aa090997b47b2bef5f597f1
noyeong <noyeong1994@gmail.com>
2024-03-28 18:20:12 +0900

    .

---
+++ 700. 쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -0,0 +1,14 @@
+// 도깨비 PJ 메인 JS - main.js ///////////////////
+
+// 모듈로 호출된 JS에서는 다른 외부 JS를 import로 호출가능!
+// import 하려는 파일에서 반드시 함수,변수등을 export해야함!
+import slideFn from "./slide.js";
+
+console.log('모듈로 메인js 호출!!',
+document.querySelector('.top-menu'));
+
+// slideFn 슬라이드 기능함수 호출 ! 
+slideFn();
+
+
+
