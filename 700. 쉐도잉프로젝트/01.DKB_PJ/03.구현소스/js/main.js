2ca6057aea3466e84d2681fc43da00289e971438
noyeong <noyeong1994@gmail.com>
2024-04-11 17:15:44 +0900

    sort()메서드 숫자문자 오름내림차순 ,gnb 데이터로뿌리기

--- 700. 쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700. 쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -13,8 +13,54 @@ import slideFn from "./slide.js";
 import * as dkbData from "../data/dkb_data.js";
 // import { previewData } from "../data/dkb_data.js";
 
+// GNB 메뉴 데이타 불러오기 ///
+import gnbData from "../data/gnb_data.js";
+console.log(gnbData);
+
 ////////////////////////////////////////////////////////////
 ////////////////구현코드파트////////////////////////////////
+
+// Gnb메뉴 코드넣기
+// 대상: .gnb
+// 데이터 :gnbData는 객체니까 배열용메서드 map()을 못씀!
+// 그래서 gnbData를 키배열로 변환해서 사용함!
+// 그리고 이 객체의 key는 상위메뉴 이기도 함!
+// Object.keys(객체) -> 해당객체의 속성명(키) 배열생성 
+console.log(Object.keys(gnbData));
+
+
+//서브메뉴가 "없음"이면 빈값 
+//아니면 서브메뉴 출력 
+// ( ${} 는 표현식자리라서 if문사용불가.
+// 바로출력되는 삼항연산자사용 
+// gnbData[키] -> 값을가져옴! 
+
+myFn.qs(".gnb").innerHTML = `
+<ul>
+   ${ Object.keys(gnbData).map(v=>`
+      <li>
+         <a href="#">${v}</a>
+         ${ gnbData[v]=="없음" ? "" : `
+         <!-- 서브메뉴 -->
+         <div class="smenu">
+            <div class="swrap">
+            <h2>${v}</h2>
+               <ol>
+                  ${ gnbData[v].map(vSub=>`
+                     <li>
+                        <a href="#">${vSub}</a>
+                     </li>
+                     `).join('')
+                  }
+               </ol>
+            </div>
+         </div>
+         `}
+      </li>
+    `).join('')
+   }
+</ul>
+`;
 
 // 1. 부드러운 스크롤 호출
 
@@ -59,6 +105,21 @@ introMv.onclick = () => {
    const previewBox = myFn.qs(".preview-box");
    // 데이터 : dkb_data.js의 previewData 배열
    const pData = dkbData.previewData;
+   // 데이터 원본의 정렬을 내림차순으로 변경!( 콘솔에 sort 찍어도 대상배열바뀜 ㅎㄷㄷ)
+   // console.log(
+
+   // 배열값인 객체의 idx키값을 기준으로
+   // 내림차순 정렬을 할때 문자형 숫자이므로
+   // Number() 숫자형변환 메서드로 싸서 숫자로써
+   // 비교하여 정확한 내림차순이 되도록 한다!
+   pData.sort((a, b) =>
+      Number(a.idx) == Number(b.idx)
+         ? 0
+         : Number(a.idx) < Number(b.idx)
+         ? 1
+         : -1
+   );
+   // );
    // 구조 : ul>li>h3+p
    // 8개만 데이터를  html로 구성하여 넣는다!
    // html 코드변수
@@ -76,13 +137,12 @@ introMv.onclick = () => {
    hcode += `</ul>`;
 
    // 데이터 확인
-   console.log(hcode);
+   // console.log(hcode);
    // console.log('대상:',previewBox,'미리보기data:', pData);
 
    // 2. 화면출력하기
    previewBox.innerHTML = hcode;
 })(); ///// 미리보기 코드랩핑구역 종료 /////////
-
 
 // 3. 현장포토 파트 내용넣기 ///////////
 ///////// 현장포토 구현 코드랩핑구역 시작 ////////
@@ -98,10 +158,10 @@ introMv.onclick = () => {
    let hcode = `<ul>`;
 
    // li구성을 hcode변수에 대입연산자로 할당함!
-   // livedata 배열은 총 8개임. 모두 돌기를 셋팅하자 
-   
+   // livedata 배열은 총 8개임. 모두 돌기를 셋팅하자
+
    lvData.forEach((v) => {
-         hcode += `
+      hcode += `
             <li>
                <figure>
                   <img src="./images/live_photo/${v.imgName}.jpg" alt="${v.title}">
@@ -114,13 +174,12 @@ introMv.onclick = () => {
    hcode += `</ul>`;
 
    // 데이터 확인
-   console.log(hcode);
+   // console.log(hcode);
    // console.log('대상:',liveBox,'현장포토data:', lvData);
 
    // 2. 화면출력하기
    liveBox.innerHTML = hcode;
 })(); ///// 현장포토 코드랩핑구역 종료 /////////
-
 
 // 3. 대표이미지 파트 내용넣기 ///////////
 ///////// 대표이미지 구현 코드랩핑구역 시작 ////////
@@ -136,10 +195,10 @@ introMv.onclick = () => {
    let hcode = `<ul>`;
 
    // li구성을 hcode변수에 대입연산자로 할당함!
-   // posterData 배열은 총 5개임. 모두 돌기를 셋팅하자 
-   
+   // posterData 배열은 총 5개임. 모두 돌기를 셋팅하자
+
    pData.forEach((v) => {
-         hcode += `
+      hcode += `
             <li>
                <figure>
                   <img src="./images/poster_img/${v.imgName}.jpg" alt="${v.title}">
@@ -152,7 +211,7 @@ introMv.onclick = () => {
    hcode += `</ul>`;
 
    // 데이터 확인
-   console.log(hcode);
+   // console.log(hcode);
    // console.log('대상:',liveBox,'대표이미지data:', pData);
 
    // 2. 화면출력하기
