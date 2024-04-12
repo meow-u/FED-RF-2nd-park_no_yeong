86a08f71d8d2665df0033ef1871ed1c2ef2e8fd1
noyeong <noyeong1994@gmail.com>
2024-04-12 16:46:24 +0900

    홈페이지 공통영역부분 데이터, js분리
    1) html 공통영역 비우기
    2) common_data.js 만들기(공통요소 데이터 객체) ->만들고 익스포트
    
    3) common.js 만들기( 공통처리 JS ) -> 만들고
    2번파일 불러오기 >>import 해당객체 from "../data/common_data.js";
    (main.js에있는 공통기능들은 여기로 이전시킴)
    + 나의함수 불러오기
    + 다른공통 함수있으면 불러오기 (슬라이드,gnb구성데이타 등등)
    
    export default function setElement(){
    만들어 익스포트하면서 대상html구역에 데이타 뿌리기
     topArea.innerHTML = comData.topArea; ......등등등
    
     불러온 다른공통함수도 호출하기 }
    
    4) main.js에서 common.js 불러오기 (최상단에서 부르고 최초호출!)

--- 700. 쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700. 쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -1,66 +1,24 @@
 // 도깨비 PJ 메인 JS - main.js ///////////////////
 
+// 공통처리함수 불러오기(html 공통영역) :가상먼저 처리한다!
+import setElement from "./common.js";
+setElement(); // 함수호출 !!!
+
 // 나의 함수 불러오기
 import myFn from "./my_function.js";
 // 부드러운 스크롤 불러오기
 import { startSS, setScrollPos } from "./smoothScroll23.js";
 
-// 모듈로 호출된 JS에서는 다른 외부 JS를 import로 호출가능!
-// import 하려는 파일에서 반드시 함수,변수등을 export해야함!
-import slideFn from "./slide.js";
+
 
 // ★★★데이터 셋팅 파일 통째로 불러오기★★ ///
 import * as dkbData from "../data/dkb_data.js";
 // import { previewData } from "../data/dkb_data.js";
 
-// GNB 메뉴 데이타 불러오기 ///
-import gnbData from "../data/gnb_data.js";
-console.log(gnbData);
+
 
 ////////////////////////////////////////////////////////////
 ////////////////구현코드파트////////////////////////////////
-
-// Gnb메뉴 코드넣기
-// 대상: .gnb
-// 데이터 :gnbData는 객체니까 배열용메서드 map()을 못씀!
-// 그래서 gnbData를 키배열로 변환해서 사용함!
-// 그리고 이 객체의 key는 상위메뉴 이기도 함!
-// Object.keys(객체) -> 해당객체의 속성명(키) 배열생성 
-console.log(Object.keys(gnbData));
-
-
-//서브메뉴가 "없음"이면 빈값 
-//아니면 서브메뉴 출력 
-// ( ${} 는 표현식자리라서 if문사용불가.
-// 바로출력되는 삼항연산자사용 
-// gnbData[키] -> 값을가져옴! 
-
-myFn.qs(".gnb").innerHTML = `
-<ul>
-   ${ Object.keys(gnbData).map(v=>`
-      <li>
-         <a href="#">${v}</a>
-         ${ gnbData[v]=="없음" ? "" : `
-         <!-- 서브메뉴 -->
-         <div class="smenu">
-            <div class="swrap">
-            <h2>${v}</h2>
-               <ol>
-                  ${ gnbData[v].map(vSub=>`
-                     <li>
-                        <a href="#">${vSub}</a>
-                     </li>
-                     `).join('')
-                  }
-               </ol>
-            </div>
-         </div>
-         `}
-      </li>
-    `).join('')
-   }
-</ul>
-`;
 
 // 1. 부드러운 스크롤 호출
 
@@ -69,8 +27,7 @@ startSS();
 // console.log('모듈로 메인js 호출!!',
 // document.querySelector('.top-menu'));
 
-// 2. slideFn 슬라이드 기능함수 호출 !
-slideFn();
+
 
 // 3. 인트로 동영상 파트 클릭시 동영상 태그 넣기
 // 이벤트 대상 == 변경대상 : .intro-mv-img
@@ -181,7 +138,7 @@ introMv.onclick = () => {
    liveBox.innerHTML = hcode;
 })(); ///// 현장포토 코드랩핑구역 종료 /////////
 
-// 3. 대표이미지 파트 내용넣기 ///////////
+// 4. 대표이미지 파트 내용넣기 ///////////
 ///////// 대표이미지 구현 코드랩핑구역 시작 ////////
 (() => {
    // 대상: .poster-data
@@ -217,3 +174,39 @@ introMv.onclick = () => {
    // 2. 화면출력하기
    posterBox.innerHTML = hcode;
 })(); ///// 대표이미지 코드랩핑구역 종료 /////////
+
+// 5. 최신동영상 파트 데이터 태그 구성하여 화면 출력하기 ///
+///////// 최신동영상파트 구현 코드랩핑구역 시작 ////////
+(()=>{
+   // 5-1. 변경대상: .clip-box
+   const clipBox = myFn.qs('.clip-box')
+   
+   // 5-2. 생성코드 변수
+   let hcode = `<ul>`;
+
+   // 데이터만큼 순회하여 li코드만들기 //
+   // 데이터: dkbData.clipData
+   dkbData.clipData.forEach(v=>{
+      hcode += `
+   <li>
+      <div class="clip-mv-box">
+         <img
+            src="./images/clip_img/${v.idx}.jpg"
+            alt="${v.subtit}"
+         />
+      </div>
+      <h4>
+         ${v.subtit}
+      </h4>
+      <h3>${v.title}</h3>
+   </li>
+      `;
+
+   }); /////////////// forEach //////////////
+
+   hcode += `</ul>`;
+
+   // 5-3. 화면출력하기 ///////
+   clipBox.innerHTML = hcode;
+
+})(); ///// 최신동영상파트 코드랩핑구역 종료 /////////
