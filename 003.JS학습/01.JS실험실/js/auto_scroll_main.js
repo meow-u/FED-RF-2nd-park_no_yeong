5fee901971843464885cd097ce6cf49ba150e75f
noyeong <noyeong1994@gmail.com>
2024-05-03 09:39:03 +0900

    .

---
+++ 003.JS학습/01.JS실험실/js/auto_scroll_main.js
@@ -0,0 +1,7 @@
+// 자동스크롤 호출하는 메인 JS - auto-scroll_main.js
+
+import AutoScroll from './auto_scroll.js';
+// 자동스크롤 함수 호출
+// -> 생성자 함수이므로 new 키워드로 호출한다!
+const scrollObj = new AutoScroll();
+// console.log(scrollObj);
