a184090338f6bd9f9a391faaef6bd17021bcfa01
noyeong <noyeong1994@gmail.com>
2024-05-21 12:51:33 +0900

    Update goods_detail.jsx
    selItem=='공유' &&  << 앞이 트루면 뒤에꺼 출력
     코드를 가져올때 최상위를 만들어서 가져오면 쉽게 셋팅할 수있다!!

--- 004.React학습/02.react-ex/js/components/goods_detail.jsx
+++ 004.React학습/02.react-ex/js/components/goods_detail.jsx
@@ -2,35 +2,66 @@
 
 // 공유신발 데이터 불러오기
 import guData from "../data/gu_data";
+// 효진드레스 데이터 불러오기
+import hjData from "../data/hj_data";
 // console.log(guData);
 
-export default function GoodsDetail({backList, gNo}) {
-    // backList- 부모컴포넌트가 전달해준 상태변수 
+export default function GoodsDetail({backList, gNo, selItem}) {
+    // (1) backList- 부모컴포넌트가 전달해준 상태변수 
     // (viewList를업데이트하는 setViewList 메서드임!)
    
-    // gNo - 상품 데이터 배열 순번 
+    // (2) gNo - 상품 데이터 배열 순번 
     // (idx 상태관리 변수가 전달됨 -> setIdx 값 변경시 컴포넌트 변경됨) 
+   
+    // (3) selItem은 부모컴포넌트에서 '공유'/ '효진' 선택코드값
+    //      selItem값으로 데이터를 선택해준다
+    //      '공유'는 guData, '효진'은 hjData
+
+    //************************************** */
+    // 선택코드에 따른 데이터 선택하기
+    const selData = 
+    selItem=='공유'? guData
+    :
+    selItem=='효진'? hjData
+    :[]
+    //************************************** */
+   
     // 코드 리턴구역
     return (
        <ol style={{display:"flex", listStyle:"none"}}>
        <li>
           <img 
-          src={'./images/vans/vans_'+guData[gNo].idx+'.jpg'} 
+          src=
+          {
+            (selItem=="공유"? './images/vans/vans_':
+            selItem=="효진"? './images/gallery/' : "")
+            + selData[gNo].idx+'.jpg'
+          } 
           alt='반스신발' style={{width:'100%'}}/>
        </li>
        <li style={{lineHeight:"2",padding:"10px",textAlign:"center"}}>
-          상품명 : {guData[gNo].gname}<br/>
-          가격 : {guData[gNo].gprice}<br/>
-          소재 : {guData[gNo].소재}<br/>
-          색상 : {guData[gNo].색상}<br/>
-          치수 : {guData[gNo].치수}<br/>
+          상품명 : {selData[gNo].gname}<br/>
+          가격 : {selData[gNo].gprice}<br/>
+         
+          {selItem=='공유' && 
+          //********************************************** */
+          // selItem=='공유' &&  << 앞이 트루면 뒤에꺼 출력
+          // 코드를 가져올때 최상위를 만들어서 가져오면 쉽게 셋팅할 수있다!! 
+          <div> 
+          소재 : {selData[gNo].소재}<br/>
+          색상 : {selData[gNo].색상}<br/>
+          치수 : {selData[gNo].치수}<br/>
           제조자/수입자 : 
-          {guData[gNo]["제조자/수입자"]}<br/>
-          제조국 : {guData[gNo].제조국}<br/>
-          제조연월 : {guData[gNo].제조연월}<br/>
+          {selData[gNo]["제조자/수입자"]}<br/>
+          제조국 : {selData[gNo].제조국}<br/>
+          제조연월 : {selData[gNo].제조연월}<br/>
           A/S 책임자와 전화번호 : <br />
-          {guData[gNo]["A/S 책임자와 전화번호"]}<br/>
-          Model : {guData[gNo].Model}<br/>
+          {selData[gNo]["A/S 책임자와 전화번호"]}<br/>
+          Model : {selData[gNo].Model}<br/>
+          </div>          
+          //********************************************** */
+          
+          }
           
        <div className='btnbx' style={{textAlign:'right', padding:'15px'}}>
           <button 
