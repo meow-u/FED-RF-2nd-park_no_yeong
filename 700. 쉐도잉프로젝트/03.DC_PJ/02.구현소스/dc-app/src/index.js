92b9d3b9b91e7000b026c72524b84af73d6316a7
noyeong <noyeong1994@gmail.com>
2024-05-30 17:06:10 +0900

    .

--- 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function MainComponent() {
+   return (
+      <>
+         <h1>하하</h1>
+      </>
+   );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//컴포넌트 출력 ////
+// 먼저 root 객체만들기ㅐ
+const root = ReactDOM
+.createRoot(document.querySelector("#root"));
+// 출력하기
+root.render(<MainComponent/>);
+
