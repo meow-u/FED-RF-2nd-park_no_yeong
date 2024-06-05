7649537300fc15f9ca43620626bda89433b2b8ab
noyeong <noyeong1994@gmail.com>
2024-06-05 16:43:45 +0900

    구조분해할당으로 객체데이터 키값 받아서 selData로 적용하기

--- 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -1,13 +1,16 @@
 //pages 폴더는 MainArea에서 바뀌어보이는 각각의 페이지
 //Main은 기본페이지
 
+import Banner from "../modules/Banner";
 
-export default function Main(){
-
-    // 코드 리턴구역
-    return(
+export default function Main() {
+   // 코드 리턴구역
+   return (
       <>
-        <h1>첫페이지</h1>
+         {/* 1. 배너컴포넌트 */}
+         <Banner catName="main1" />
+         {/* 배너컴포넌트 */}
+         <Banner catName="CHARACTERS" />
       </>
-    );
-}//////////// Main ////////////////////////
+   );
+} //////////// Main ////////////////////////
