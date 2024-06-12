d7246ab25f166694154a32d9e3426d9fcdddd5c8
noyeong <noyeong1994@gmail.com>
2024-06-12 15:19:11 +0900

    DC 클릭시 모달로 비디오 플레이 시키기

--- 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
+++ 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
@@ -1,12 +1,22 @@
 // 스와이퍼 플러그인 컴포넌트
 
-import React from "react";
+import React, { useEffect } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+// 제이쿼리 불러오기 
+import $ from 'jquery';
 
 // Import Swiper styles : 모듈용 기본 css파일 로딩
 import "swiper/css";
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
+
+// 폰트어썸 불러오기
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// 종류는 3가지
+import { faOtter, faTree } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+import { faCcApplePay } from "@fortawesome/free-brands-svg-icons";
+
 
 // 스와이퍼 비디오모듈 css : 내가작성한 css
 import "./css/swiper_vid.scss";
@@ -18,12 +28,47 @@ import { swVidData } from "../data/swiper_vid";
 // (여기서는 네비게이션 - 양쪽이동버튼 )
 import { Navigation } from "swiper/modules";
 
-export function SwiperVid({ catName }) {   ///////////////////////////
+export function SwiperVid({ catName }) {//////////////////////////
+   ///////////////////////////
    // catName은 카테고리 이름
 
    // 선택데이터 변수할당
    const selData = swVidData[catName];
 
+   // 비디오 보이기함수 /////
+const showVideo = (src,tit)=>{
+   console.log('비디오 보여!',src,tit);
+
+// 1. 대상선정
+    // 1-1. 아이프레임 : .play-vid iframe
+    const ifr = $('.play-vid iframe');
+    // 1-2. 전체 박스 : .vid-bx
+    const vbx = $('.vid-bx');
+    // 1-3. 타이틀 박스 : .ifr-tit
+    const itit = $('.ifr-tit');
+    // 1-4. 닫기 버튼 : .cbtn
+    const cbtn = $('.cbtn');
+
+   // 2. 변경하기
+   // 2-1. 아이프레임 src 경로넣기
+   ifr.attr('src',src+'?autoplay=1');
+   // 2-2. 비디오 타이틀 넣기
+   itit.text(tit);
+   // 2-3. 박스보이기
+   // 2-4. 닫기버튼셋팅
+   cbtn.on('click',()=>{
+      // 전체박스 사라지기
+      vbx.fadeOut(300);
+      // 기존 동영상  플레이 멈추기 (태그의 src 속성을 삭제함)
+      ifr.attr("src","");
+   });
+
+
+   // 박스보이기
+   vbx.fadeIn(300);
+
+}; ////////// show video 함수 //////
+   
    return (
       <>
          <Swiper
@@ -36,11 +81,27 @@ export function SwiperVid({ catName }) {   ///////////////////////////
          >
             {selData.map((v, i) => (
                <SwiperSlide key={i}>
-                  <section className="sw-inbox">
+                  <section className="sw-inbox"
+                  /* **************************** */
+                  /* js의 addEventLisenner랑 같음 */
+                  onClick={()=>showVideo(v.vsrc,v.tit)}>
                      {/* 동영상이미지박스 */}
                      <div className="vid-img">
                         <img src={v.isrc} alt={v.tit} />
                         {/* 폰트어썸 아이콘 */}
+                        <FontAwesomeIcon
+                           icon={faOtter}
+                           style={{
+                              position: "absolute",
+                              bottom: "55%",
+                              left: "10%",
+                              color: "#fff",
+                              fontSize: "75px",
+                           }}
+                        />
+                        <FontAwesomeIcon icon={faTree} />
+                        <FontAwesomeIcon icon={faCirclePlay} />
+                        <FontAwesomeIcon icon={faCcApplePay} />
                      </div>
                      {/* 동영상 타이틀 박스 */}
                      <div className="vid-tit">
