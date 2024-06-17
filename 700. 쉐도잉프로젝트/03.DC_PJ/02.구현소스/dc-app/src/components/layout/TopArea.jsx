08199d3ac4ab4074656066f59e23b84bdb424bec
noyeong <noyeong1994@gmail.com>
2024-06-17 14:36:05 +0900

    top-area에 검색창만들기

--- 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -9,6 +9,9 @@ import { menu } from "../data/gnb";
 // 상단영역 css불러오기
 import "../../css/top_area.scss";
 import Logo from "../modules/Logo";
+// 폰트어썸
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function TopArea() {
    // 이동함수
@@ -16,11 +19,11 @@ export default function TopArea() {
    // 사용시 goNav(라우터주소, {전달객체})
    // 전달객체 없으면 비워놓음!
    // 사용법: 반드시 useNavigate()메서드를 변수에 담아
-    // 이동할 라우터 주소를 쓰면 이동한다
-    // 예) goNav('/news') -> 뉴스페이지이동
-    // 예) goNav('/') -> 첫페이지이동
-    // 이동주소는 대소문자 구분없음!
-    // 슬래쉬 없이 써도 루트로 인식함
+   // 이동할 라우터 주소를 쓰면 이동한다
+   // 예) goNav('/news') -> 뉴스페이지이동
+   // 예) goNav('/') -> 첫페이지이동
+   // 이동주소는 대소문자 구분없음!
+   // 슬래쉬 없이 써도 루트로 인식함
    // --> 알고있어야하지만 link to가 좋다..
 
    // 코드 리턴구역
@@ -77,6 +80,30 @@ export default function TopArea() {
                         }
                      </li>
                   ))}
+                  {/* 3. 검색,회원가입, 로그인 링크 */}
+                  <li style={{
+                     marginLeft:'auto', /* 끝에붙이기  */
+                     marginRight:'25px'
+                     }}>
+                        {/* 검색입력박스 */}
+                        <div className="searchingGnb"
+                        style={{display:'block'}}>
+                           {/* 검색버튼 돋보기 아이콘 */}
+                           <FontAwesomeIcon 
+                           icon={faSearch} 
+                           className="schbtnGnb"
+                           title="open search"
+                           />
+                           {/* 입력창 */}
+                           <input 
+                           type="text" 
+                           name="schinGnb"
+                           id="schinGnb"
+                           placeholder=""
+                           />
+                        </div>
+
+                  </li>
                </ul>
             </nav>
          </header>
