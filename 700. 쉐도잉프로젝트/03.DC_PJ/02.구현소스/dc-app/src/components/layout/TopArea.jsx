f100992b860c248d60a1f960a225c1d4b6e93bf1
noyeong <noyeong1994@gmail.com>
2024-06-17 11:52:53 +0900

    useNavigate() 사용법

--- 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700. 쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -2,7 +2,7 @@
 
 /* 3 */
 // 어느하위컴포넌트에 있어도 이걸로 작동시킴
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 //gnb 데이터 불러오기
 import { menu } from "../data/gnb";
 
@@ -11,6 +11,18 @@ import "../../css/top_area.scss";
 import Logo from "../modules/Logo";
 
 export default function TopArea() {
+   // 이동함수
+   const goNav = useNavigate();
+   // 사용시 goNav(라우터주소, {전달객체})
+   // 전달객체 없으면 비워놓음!
+   // 사용법: 반드시 useNavigate()메서드를 변수에 담아
+    // 이동할 라우터 주소를 쓰면 이동한다
+    // 예) goNav('/news') -> 뉴스페이지이동
+    // 예) goNav('/') -> 첫페이지이동
+    // 이동주소는 대소문자 구분없음!
+    // 슬래쉬 없이 써도 루트로 인식함
+   // --> 알고있어야하지만 link to가 좋다..
+
    // 코드 리턴구역
    return (
       <>
@@ -23,9 +35,18 @@ export default function TopArea() {
                <ul>
                   {/* 1. 로고 컴포넌트 */}
                   <li>
-                     <Link to="/">
+                     <a
+                        href="#"
+                        onClick={(e) => {
+                           e.preventDefault(); /* #출력안되게 기본기능막 */
+                           goNav(""); /* 슬래쉬없어도 루트로인식 */
+                        }}
+                     >
                         <Logo logoStyle="top" />
-                     </Link>
+                     </a>
+                     {/* <Link to="/">
+                        <Logo logoStyle="top" />
+                     </Link> */}
                   </li>
                   {/* 2. Gnb메뉴 데이터 배열로 만들기 */}
                   {menu.map((v, i) => (
